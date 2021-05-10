ea86eac7f8c524f95902074d629874c889a102db
stefan-think <stefan016c@gmail.com>
2021-05-10 12:22:27 -0400

    - Now filter method is used for partitionBooksByBorrowedStatus()
    - Comments added

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,29 +1,31 @@
+//Return the account object when given a particular ID
 function findAccountById(accounts, id) {
   return accounts.find(account => account.id === id);
 }
 
+//Return the list of accounts ordered by last name
 function sortAccountsByLastName(accounts) {
   return accounts.sort((accountA, accountB) => accountA.name.last > accountB.name.last ? 1 : -1);
 }
 
+//Return the number of times an account has created a 'borrow' record
 function getTotalNumberOfBorrows(account, books) {
   const accountId = account.id
   let total = 0;
   books.forEach(book => book.borrows.forEach(borrow => accountId === borrow.id && total++));
   return total;
 }
- 
 
+//Return all of the books taken out by an account with the author embedded
 function getBooksPossessedByAccount(account, books, authors) {
   let booksPossessed = [];
   books.forEach(book => {
     let borrowedBook = book.borrows;
-    //try to use filter instead if
     if (borrowedBook.find(borrow => borrow.id === account.id && borrow.returned === false)) {
       booksPossessed.push(book);
     }
   })
-  
+
   booksPossessed.forEach(book=>{
     let author = authors.find(person => person.id === book.authorId);
     book['author'] = author;
--- public/src/books.js
+++ public/src/books.js
@@ -1,27 +1,22 @@
+//return the author object when given a particular ID
 function findAuthorById(authors, id) {
   return authors.find(author => author.id === id);
 }
 
+//return the book object when given a particular ID
 function findBookById(books, id) {
   return books.find(book => book.id === id);
 }
 
+//return an array with two arrays: borrowed books and returned books
 function partitionBooksByBorrowedStatus(books) {
-  let availableBooks = [];
-  let unavailableBooks = [];
-  const bookStatus = [];
-  books.forEach(book => {
-    let borrowedBook = book.borrows;
-    if (borrowedBook.find(borrow => borrow.returned === false)) {
-      unavailableBooks.push(borrowedBook);
-    } else {
-      availableBooks.push(borrowedBook);
-    }
-  });
-  bookStatus.push(unavailableBooks, availableBooks);
-  return bookStatus;
+  const available = books.filter((book) => book.borrows[0].returned);
+  const unavailable = books.filter((book) => !book.borrows[0].returned);
+  return [unavailable, available];
 }
 
+//return an array for a book of all borrowers with their information and return status
+//number of borrowers is limited to 10
 function getBorrowersForBook(book, accounts) {
   const {borrows} = book;
   const borrowers = borrows.map(({ id, returned })=> {
@@ -29,7 +24,7 @@ function getBorrowersForBook(book, accounts) {
     return {...account, returned,};
   });
   return borrowers.slice(0, 10);
-  
+
 }
 
 module.exports = {
